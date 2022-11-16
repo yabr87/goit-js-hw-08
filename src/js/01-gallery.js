c27649370abe5860ab43df91ee9ee2560ac98708
yabr87 <108272773+yabr87@users.noreply.github.com>
2022-11-16 22:53:25 +0200

    add 1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
-// Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryListItem = document.querySelector('.gallery');
+
+//add markup________________________________________
+const createGalleryListItem = galleryItems
+  .map(
+    ({
+      preview,
+      original,
+      description,
+    }) => `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+  )
+  .join('');
+
+galleryListItem.insertAdjacentHTML('afterbegin', `${createGalleryListItem}`);
+
+// open simpleLightbox
+
+const gallerySimpleLightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
