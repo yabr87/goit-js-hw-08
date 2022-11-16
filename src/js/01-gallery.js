24f310725af8d9e7add01fb71f2669832a76273d
yabr87 <108272773+yabr87@users.noreply.github.com>
2022-11-16 23:01:23 +0200

    fix task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,5 @@
-import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import SimpleLightbox from 'simplelightbox';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
