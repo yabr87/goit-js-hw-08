d66b18f80b8093db8d2e977213c136d9350cd53c
yabr87 <108272773+yabr87@users.noreply.github.com>
2022-11-18 10:53:32 +0200

    task2, 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,46 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+import { setStorage, getStorage, removeStorage } from './storage';
 
+// const key = 'ключ';
+// setStorage(key, 'це значення ключа'); //записали сховище
+// console.log(getStorage(key)); // отримали значення з сховища
+// removeStorage(key); // видалили значення з сховища
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const stotageKey = 'videoplayer-current-time';
+let currentTime = getStorage(stotageKey);
+
+const onPlay = function (data) {
+  setStorage(stotageKey, data.seconds);
+  return;
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+// player.setCurrentTime(currentTime);
+
+player.setCurrentTime(currentTime).catch(function (error) {
+  setStorage(stotageKey, '0');
+  return;
+});
+
+// player
+//   .setCurrentTime(30.456)
+//   .then(function (seconds) {
+//     // seconds = the actual time that the player seeked to
+//     // щось можна виконати seconds
+//   })
+//   .catch(function (error) {
+//       switch (error.name) {
+//         // щось виконуемо якщо піймали помилку.
+//       case 'RangeError':
+//         // the time was less than 0 or greater than the video’s duration
+//         break;
+
+//       default:
+//         // some other error occurred
+//         break;
+//     }
+//   });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+import throttle from 'lodash.throttle';
+import { setStorage, getStorage, removeStorage } from './storage';
 
+const form = document.querySelector('.feedback-form');
+const stotageKey = 'feedback-form-state';
+const storage = getStorage(stotageKey);
+
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+function onFormInput(event) {
+  const {
+    elements: { email, message },
+  } = event.currentTarget;
+
+  setStorage(stotageKey, { Email: email.value, Message: message.value });
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, message },
+  } = event.currentTarget;
+
+  if (email.value === '' || message.value === '') {
+    return alert('Будь ласка заповніть всі поля!');
+  }
+
+  console.log({ Email: email.value, Message: message.value });
+  event.currentTarget.reset();
+
+  removeStorage(stotageKey);
+}
+
+function containsАormValues() {
+  if (!storage) return;
+  form.elements.email.value = storage.Email;
+  form.elements.message.value = storage.Message;
+}
+
+containsАormValues();
