e3333d5b196c2a406ce4f72289b016b16d560fcd
yabr87 <108272773+yabr87@users.noreply.github.com>
2022-11-18 16:11:49 +0200

    fix task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,19 +4,27 @@ import { setStorage, getStorage, removeStorage } from './storage';
 const form = document.querySelector('.feedback-form');
 const stotageKey = 'feedback-form-state';
 const storage = getStorage(stotageKey);
+let emailValue = '';
+let messageValue = '';
 
 form.addEventListener('input', throttle(onFormInput, 500));
 form.addEventListener('submit', onFormSubmit);
 
 function onFormInput(event) {
-  const {
-    elements: { email, message },
-  } = event.currentTarget;
+  // отримуємо та записуємо данні в сховище
+  if (event.target.name === 'email') {
+    emailValue = event.target.value;
+  }
 
-  setStorage(stotageKey, { Email: email.value, Message: message.value });
+  if (event.target.name === 'message') {
+    messageValue = event.target.value;
+  }
+
+  setStorage(stotageKey, { Email: emailValue, Message: messageValue });
 }
 
 function onFormSubmit(event) {
+  //кнопка submit. перевіряємо поля, вивонимо в констоль,  очищуемо форму
   event.preventDefault();
   const {
     elements: { email, message },
@@ -33,6 +41,7 @@ function onFormSubmit(event) {
 }
 
 function containsАormValues() {
+  // заповнюєо форму після завантаження
   if (!storage) return;
   form.elements.email.value = storage.Email;
   form.elements.message.value = storage.Message;
